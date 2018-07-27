677f263042d82935b9adb9c1f0e54b455476699b
Bruno Wagner <brunowagner62@gmail.com>
2018-07-27 00:48:04 -0300

    Nao consegui resolver o problem, porem dei uma atualizada no projeto infra.ORM

---
+++ ws-banco-tabajara/Ws-BancoTabajara/Ws-BancoTabajara.Infra.ORM/Initializer/DbInitializer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ws_BancoTabajara.Infra.ORM.Base;
+using Ws_BancoTabajara.Infra.ORM.Migrations;
+
+namespace Ws_BancoTabajara.Infra.ORM.Initializer
+{
+    class DbInitializer : MigrateDatabaseToLatestVersion<BancoTabajaraDbContext, Configuration>
+    {
+    }
+}
